c6d0640f092277ef18d9ac09aa2ebc94053b9028
santo-it <santo_leto@yahoo.it>
2016-11-02 09:17:59 +0100

    Script & configuration files
    Script & configuration files

---
+++ scripts/orientdb-neo4j-importer.bat
@@ -0,0 +1,49 @@
+@echo off
+rem
+rem Copyright (c) Orient Technologies LTD (http://www.orientechnologies.com)
+rem
+rem Guess ORIENTDB_HOME if not defined
+set CURRENT_DIR=%cd%
+
+if exist "%JAVA_HOME%\bin\java.exe" goto setJavaHome
+set JAVA="java"
+goto okJava
+
+:setJavaHome
+set JAVA="%JAVA_HOME%\bin\java"
+
+:okJava
+if not "%ORIENTDB_HOME%" == "" goto gotHome
+set ORIENTDB_HOME=%CURRENT_DIR%
+if exist "%ORIENTDB_HOME%\bin\orientdb-neo4j-importer.bat" goto okHome
+cd ..
+set ORIENTDB_HOME=%cd%
+cd %CURRENT_DIR%
+
+:gotHome
+if exist "%ORIENTDB_HOME%\bin\orientdb-neo4j-importer.bat" goto okHome
+echo The ORIENTDB_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
+
+:okHome
+rem Get remaining unshifted command line arguments and save them in the
+set CMD_LINE_ARGS=
+
+SET NEO4JLIB=%~2%
+
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+
+:doneSetArgs
+set LOG_FILE=%ORIENTDB_HOME%\config\orientdb-neo4j-importer-log.properties
+
+set JAVA_MAX_DIRECT=-XX:MaxDirectMemorySize=2g
+set JAVA_OPTS_SCRIPT= %JAVA_MAX_DIRECT% -Djava.util.logging.config.file=%LOG_FILE%
+
+call %JAVA% -client -classpath "%NEO4JLIB%\*;%ORIENTDB_HOME%\lib\*" %JAVA_OPTS_SCRIPT% com.orientechnologies.orient.neo4jimporter.ONeo4jImporter %CMD_LINE_ARGS%
+
+:end
---
+++ scripts/orientdb-neo4j-importer.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+# Only set ORIENTDB_HOME if not already set
+[ -f "$ORIENTDB_HOME"/lib/orientdb-neo4j-importer-@VERSION@.jar ] || ORIENTDB_HOME=`cd "$PRGDIR/.." ; pwd`
+export ORIENTDB_HOME
+
+# Set JavaHome if it exists
+if [ -f "${JAVA_HOME}/bin/java" ]; then
+   JAVA=${JAVA_HOME}/bin/java
+else
+   JAVA=java
+fi
+export JAVA
+
+LOG_FILE=$ORIENTDB_HOME/config/orientdb-neo4j-importer-log.properties
+JAVA_MAX_DIRECT="-XX:MaxDirectMemorySize=2g"
+JAVA_OPTS_SCRIPT="$JAVA_MAX_DIRECT -Djava.util.logging.config.file=$LOG_FILE"
+
+ARGS='';
+for var in "$@"; 
+	do ARGS="$ARGS $var"    
+done
+
+#gets value of neo4jlibdir, so that we can use it in the -classpath
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -neo4jlibdir)
+    NEO4JLIB="$2"
+    shift 
+    ;;
+    
+    *)
+    # unknown option
+    ;;
+esac
+shift  
+done
+
+#if [ -z ${NEO4JLIB+x} ]; then 
+#	echo "Option -neo4jlibdir is mandatory" 
+#	echo "Exiting"
+#	exit 1
+#else 
+#fi
+
+exec "$JAVA" -client -cp "$NEO4JLIB/*:$ORIENTDB_HOME/lib/*" $JAVA_OPTS_SCRIPT  \
+    com.orientechnologies.orient.neo4jimporter.ONeo4jImporter $ARGS
